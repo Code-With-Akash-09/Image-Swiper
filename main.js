83e5f2e94575be0546886256710f2d09a860d811
Akash Lakade <akashlakade2001@gmail.com>
2023-03-17 10:14:14 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,18 @@
+var swiper = new Swiper(".mySwiper", {
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+      delay: 3000,
+      disableOnInteraction: false,
+    },
+    loop: true,
+    effect: "fade",
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
